02a31abd88d7ae5fae7e4d12d61476ca034c9b39
Arjun-tp <arjuntpnambiar@gmail.com>
2025-03-12 13:00:14 -0400

    feat: integrated google distance matrix api and updated codebase structure

---
+++ backend/config/constant.js
@@ -0,0 +1,6 @@
+export const responseMessage = {}
+
+export const apiUrls = {
+  GOOGLE_URL: 'https://routes.googleapis.com/directions/v2:computeRoutes',
+  NOMINATIM_URL: 'https://nominatim.openstreetmap.org/search',
+}
