31b0d1625e706f3ca3475d7cc42d22512057fa03
Arjun-tp <arjuntpnambiar@gmail.com>
2025-03-12 20:39:10 -0400

    fix: deployment allowed list

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		allowedHosts: [
+			'address-mapper-production-322e.up.railway.app',
+			// 'https://address-mapper-production-322e.up.railway.app',
+			// 'http://address-mapper-production-322e.up.railway.app',
+		]
+	}
 });
